7ee0a8a9e79e95210b9bb878a583f1c2985a5161
Vladimir Keleshev <halst@ya.ru>
2013-03-18 21:26:01 +0100

    Initial commit

---
+++ test_value.py
@@ -0,0 +1,61 @@
+from pytest import raises
+
+from value import Value
+
+
+def fixture(init):
+    class Option(Value):
+        __init__ = init
+    return Option
+
+
+def test_primitive_value():
+    Option = fixture(lambda self: None)
+    assert repr(Option()) == 'Option()'
+    assert hash(Option()) == hash('Option()')
+    assert Option() == Option()
+    assert not Option() != Option()
+    assert 'self' not in Option().__dict__
+
+
+def test_init_without_defaults():
+    Option = fixture(lambda self, short, long: None)
+    for option in [Option('-a', '--all'),
+                   Option(short='-a', long='--all'),
+                   Option('-a', long='--all')]:
+        assert repr(option) == "Option('-a', '--all')"
+        assert hash(option) == hash("Option('-a', '--all')")
+        assert option == Option('-a', '--all')
+        assert option.short == '-a'
+        assert option.long == '--all'
+        assert 'self' not in option.__dict__
+
+
+def test_init_with_defaults():
+    Option = fixture(lambda self, short, long='--default': None)
+    assert repr(Option('-a')) == "Option('-a')"
+    assert repr(Option('-a', '--default')) == "Option('-a')"
+    assert repr(Option('-a', '--all')) == "Option('-a', '--all')"
+    assert 'self' not in Option('-a').__dict__
+
+
+def test_corner_case_of_self_with_default():
+    Option = fixture(lambda self='<self>', short='-a': None)
+    assert repr(Option('-a')) == "Option()"
+    assert repr(Option('-b')) == "Option('-b')"
+    assert 'self' not in Option('-a').__dict__
+
+
+def test_varargs_and_kwargs_are_not_allowed():
+    Option = fixture(lambda self, *args: None)
+    with raises(ValueError):
+        Option()
+    Option = fixture(lambda self, **kwargs: None)
+    with raises(ValueError):
+        Option()
+
+def test_tuple_parameter_unpacking_is_not_allowed():
+    Option = fixture(lambda self, (one, two): None)
+    with raises(ValueError):
+        Option()
+
---
+++ value.py
@@ -0,0 +1,38 @@
+from inspect import getargspec
+
+
+class Value(object):
+
+    def __new__(class_, *args, **kwargs):
+        self = object.__new__(class_, *args, **kwargs)
+        para, varargs, keywords, defaults = getargspec(self.__init__)
+        if varargs:
+            raise ValueError('`*args` are not allowed in __init__')
+        if keywords:
+            raise ValueError('`**kwargs` are not allowed in __init__')
+        if not all(type(p) is str for p in para):
+            raise ValueError('parameter unpacking is not allowed in __init__')
+        defaults = () if not defaults else defaults
+        self.__dict__.update(dict(zip(para[:0:-1], defaults[::-1])))
+        self.__dict__.update(dict(zip(para[1:], args) + kwargs.items()))
+        return self
+
+    def __repr__(self):
+        null = object()
+        para, _, _, defaults = getargspec(self.__init__)
+        para = para[1:]
+        actuals = [getattr(self, p) for p in para]
+        defaults = () if not defaults else defaults
+        defaults = defaults[1:] if len(defaults) == len(para) + 1 else defaults
+        defaults = (null,) * (len(para) - len(defaults)) + defaults
+        return '%s(%s)' % (self.__class__.__name__,
+                ', '.join(repr(a) for a, d in zip(actuals, defaults) if a != d))
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __ne__(self, other):
+        return repr(self) != repr(other)
